c697651bc1ab3235f1caf7622273e1577e752a12
Zlata P <zllataap@gmail.com>
2021-12-19 13:57:48 +0300

    first master commit

---
+++ lab.2.1/lab.2.1.cpp
@@ -0,0 +1,28 @@
+﻿// Lab_02.cpp 
+// < Половка Злата > 
+// Лабораторна робота № 2. 
+// Лінійні програми. 
+// Варіант 0.6
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	double a; // вхідний параметр
+	double z1; //результат обчислення 1-го виразу
+	//double z2; //результат обчислення 2-го виразу
+
+	cout << "a = "; cin >> a;
+	z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+	//z2 = 4 * cos(a / 2) * cos((5.0 * a) / 2.0) * cos(4 * a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
